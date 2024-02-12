8e9a1802e0382be6be9496e681d3417e7b35919d
vishaljadhav07 <vishal.jadhav@researchwire.in>
2024-02-12 15:07:28 +0530

    TestNG Basics

---
+++ Learn_TestNG/src/main/java/New/Learn_TestNG/App.java
@@ -0,0 +1,13 @@
+package New.Learn_TestNG;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Learn_TestNG/src/test/java/New/_01_Installation.java
@@ -0,0 +1,10 @@
+package New;
+
+public class _01_Installation {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
